aa45f04ac2a10e2e99a26d3927539d1ef36fae35
nolandex <stemapal1234@gmail.com>
2025-10-21 11:03:34 +0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,43 +1,56 @@
-import React from 'react';
 import Document, {
-   Html,
-   Head,
-   Main,
-   NextScript,
-   DocumentContext,
-   DocumentInitialProps,
-} from 'next/document';
-import {CssBaseline} from '@nextui-org/react';
-import {globalStyles} from '../styles/global.stitches';
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
+import React from "react";
 
-class MyDocument extends Document {
-   static async getInitialProps(
-      ctx: DocumentContext
-   ): Promise<DocumentInitialProps> {
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
       const initialProps = await Document.getInitialProps(ctx);
+
       return {
-         ...initialProps,
-         styles: React.Children.toArray([initialProps.styles]),
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
       };
-   }
+    } finally {
+      sheet.seal();
+    }
+  }
 
-   render() {
-      return (
-         <Html lang="en">
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link
-               href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
-               rel="stylesheet"
-            />
-            <Head>{CssBaseline.flush()}</Head>
-
-            <body>
-               <Main />
-               <NextScript />
-            </body>
-         </Html>
-      );
-   }
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
-
-export default MyDocument;

4d8b188c8e8e39bdb0d8a7fab78f132a91425352
nolandex <stemapal1234@gmail.com>
2025-10-21 11:08:24 +0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,39 +1,23 @@
+import React from 'react';
 import Document, {
   Html,
   Head,
   Main,
   NextScript,
   DocumentContext,
-} from "next/document";
-import { ServerStyleSheet } from "styled-components";
-import React from "react";
+  DocumentInitialProps,
+} from 'next/document';
+import { CssBaseline } from '@nextui-org/react';
 
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: React.Children.toArray([initialProps.styles]),
+    };
   }
 
   render() {
@@ -45,6 +29,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
+          {CssBaseline.flush()}
         </Head>
         <body>
           <Main />
@@ -54,3 +39,5 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+export default MyDocument;

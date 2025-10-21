c48fb287fb30d7c405a415c042d1f5c0c5d386b3
nolandex <stemapal1234@gmail.com>
2025-10-21 11:11:59 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,37 +1,35 @@
 import '../styles/globals.css';
-import type {AppProps} from 'next/app';
-import {createTheme, NextUIProvider} from '@nextui-org/react';
-import {ThemeProvider as NextThemesProvider} from 'next-themes';
+import type { AppProps } from 'next/app';
+import { createTheme, NextUIProvider, CssBaseline } from '@nextui-org/react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
 const lightTheme = createTheme({
-   type: 'light',
-   theme: {
-      colors: {},
-   },
+  type: 'light',
+  theme: { colors: {} },
 });
 
 const darkTheme = createTheme({
-   type: 'dark',
-   theme: {
-      colors: {},
-   },
+  type: 'dark',
+  theme: { colors: {} },
 });
 
-function MyApp({Component, pageProps}: AppProps) {
-   return (
-      <NextThemesProvider
-         defaultTheme="system"
-         attribute="class"
-         value={{
-            light: lightTheme.className,
-            dark: darkTheme.className,
-         }}
-      >
-         <NextUIProvider>
-            <Component {...pageProps} />
-         </NextUIProvider>
-      </NextThemesProvider>
-   );
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <NextThemesProvider
+      defaultTheme="system"
+      attribute="class"
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className,
+      }}
+    >
+      {/* Tambahkan CssBaseline di sini */}
+      <NextUIProvider>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </NextThemesProvider>
+  );
 }
 
 export default MyApp;
